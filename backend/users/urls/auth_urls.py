5c4e4718fa228a3a4a91694b0b5f96029be3b6a6
Duong Qui On <quyonduong@gmail.com>
2025-03-21 16:28:22 +0700

    feat(backend): Create the reset password feature for user views

--- backend/users/urls/auth_urls.py
+++ backend/users/urls/auth_urls.py
@@ -3,4 +3,5 @@ from users.views import auth_views as auth
 
 urlpatterns = [
     path('change-password/', auth.change_password, name="change-password"),
+    path("set-init-password/", auth.set_password, name="set-init-password"),
 ]
